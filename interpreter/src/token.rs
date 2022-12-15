2ec866d96e481cc94c3559878b7e9e462ac3745c
Nahua Kang <n.kang@opentalk.eu>
2022-12-15 16:32:37 +0100

    4.2 Lexemes and Tokens

--- interpreter/src/token.rs
+++ interpreter/src/token.rs
@@ -1,14 +1,75 @@
-#[derive(Debug)]
+use std::fmt;
+
+#[derive(Clone, Debug, PartialEq)]
 pub enum TokenType {
-    // TODO:  Add enough TokenType variants to compile. Implement in 4.2.
+    // Single-character tokens
     LeftParen,
     RigthParen,
+    LeftBrace,
+    RightBrace,
+    Comma,
+    Dot,
+    Minus,
+    Plus,
+    Semicolon,
+    Slash,
+    Star,
+    // One or two character tokens
+    Bang,
+    BangEqual,
+    Equal,
+    EqualEqual,
+    Greater,
+    GreaterEqual,
+    Less,
+    LessEqual,
+    // Literals
+    Identifier,
+    String { literal: String },
+    Number { literal: f64 },
+    // Keywords
+    And,
+    Class,
+    Else,
+    False,
+    Fun,
+    For,
+    If,
+    Nil,
+    Or,
+    Print,
+    Return,
+    Super,
+    This,
+    True,
+    Var,
+    While,
+    EOF,
 }
 
-#[derive(Debug)]
+#[derive(Clone, Debug, PartialEq)]
 pub struct Token {
-    // TODO: Add enough Token variants to compile. Implement in 4.2.
     pub tpe: TokenType,
     pub lexeme: String,
     pub line: i32,
+}
+
+impl Token {
+    pub fn new(tpe: TokenType, lexeme: &str, line: i32) -> Self {
+        Self {
+            tpe,
+            lexeme: lexeme.to_string(),
+            line,
+        }
+    }
+}
+
+impl fmt::Display for Token {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match &self.tpe {
+            TokenType::String { literal } => write!(f, "String {:?} {:?}", self.lexeme, literal),
+            TokenType::Number { literal } => write!(f, "Number {:?} {:?}", self.lexeme, literal),
+            _ => write!(f, "{:?} {:?}", self.tpe, self.lexeme),
+        }
+    }
 }
